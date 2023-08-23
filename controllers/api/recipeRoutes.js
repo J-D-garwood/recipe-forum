480dc3c896cd854bc4f5e455e351ffcbca0260af
J_D_Nye <garwoodjdm@gmail.com>
2023-08-23 14:43:12 +1000

    added recipe routing and auth.js in helpers folder

---
+++ controllers/api/recipeRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { Recipe } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newRecipe = await Recipe.create({
+            ...req.body,
+            userId: req.session.userId,
+        });
+        res.status(200).json(newRecipe);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const recipeData = await Recipe.destroy({
+            where: {
+                id: req.params.id,
+                userId: req.session.userId,
+            },
+        });
+
+        if (!recipeData) {
+            res.status(404).json({ message: 'No recipe found with this id' });
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
