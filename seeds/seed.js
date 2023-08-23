74898589aa3948460cb30952ef057d0cab8ac84f
shimna <chimmu.p@gmail.com>
2023-08-23 23:14:11 +1000

    updated seed data for recipes

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,10 +7,13 @@ const recipeData = require('./recipeData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  //seeding the database with users
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  //seeding the database with recipes
   const recipes = await Recipe.bulkCreate(recipeData);
   process.exit(0);
 };
