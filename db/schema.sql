fb74f39db4f4417c90621750dbade9ab704bd8ed
shimna <chimmu.p@gmail.com>
2023-08-23 16:05:07 +1000

    updated db name

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS recipeforum_db;
+DROP DATABASE IF EXISTS recipe_db;
 
-CREATE DATABASE recipeforum_db;
+CREATE DATABASE recipe_db;
---
+++ seeds/seed.js
@@ -0,0 +1,18 @@
+const sequelize = require('../config/connection');
+const { User, Recipe } = require('../models');
+
+const userData = require('./userData.json');
+const recipeData = require('./recipeData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const recipes = await Recipe.bulkCreate(recipeData);
+  process.exit(0);
+};
+
+seedDatabase();
