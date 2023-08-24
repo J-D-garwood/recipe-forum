3e096706683556f2a42197f6dd30c1ce8393f60d
J_D_Nye <garwoodjdm@gmail.com>
2023-08-24 19:20:48 +1000

    added home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { Recipe, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const RecipeData = await Recipe.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                }
+            ],
+        });
+
+        const recipes = RecipeData.map((recipe) => recipe.get({ plain: true }));
+
+        res.render('homepage', {
+            recipes,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/recipe/:id', async (req, res) => {
+    try {
+        const recipeData = await Recipe.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ]
+        })
+        const recipe = recipeData.get({plain: true});
+        res.render('recipe', {
+            ...recipe,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Recipe }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return
+    }
+    res.render('login');
+  });
+
+  module.exports = router;
